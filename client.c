510c6085f948081b86a1a04accda4817db520f80
davhak2 <davidhakobyan014@gmail.com>
2025-05-11 01:23:33 +0400

    only logic here

---
+++ client.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define BUF_SIZE 1024
+
+int main(int argc, char **argv)
+{
+	int sock;
+	struct sockaddr_in serv_addr;
+	char buffer[BUF_SIZE];
+
+	if (argc != 3)
+	{
+		printf("Usage: %s <IP> <Port>\n", argv[0]);
+		return 1;
+	}
+
+	sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock == -1) {
+		perror("socket");
+		return 1;
+	}
+
+	memset(&serv_addr, 0, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(atoi(argv[2]));
+
+	if (inet_pton(AF_INET, argv[1], &serv_addr.sin_addr) <= 0) {
+		perror("inet_pton");
+		close(sock);
+		return 1;
+	}
+
+	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1) {
+		perror("connect");
+		close(sock);
+		return 1;
+	}
+
+	printf("Enter a string: ");
+	if (fgets(buffer, BUF_SIZE, stdin) == NULL) {
+		perror("fgets");
+		close(sock);
+		return 1;
+	}
+	buffer[strcspn(buffer, "\n")] = 0;
+
+	if (send(sock, buffer, strlen(buffer), 0) == -1) {
+		perror("send");
+		close(sock);
+		return 1;
+	}
+
+	memset(buffer, 0, BUF_SIZE);
+	if (recv(sock, buffer, BUF_SIZE - 1, 0) == -1) {
+		perror("recv");
+		close(sock);
+		return 1;
+	}
+
+	printf("Modified string from server: %s\n", buffer);
+	close(sock);
+	return 0;
+}
+
---
+++ server.c
@@ -0,0 +1,92 @@
+// server.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#define BUF_SIZE 1024
+
+void process_string(char *str)
+{
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] == 'z' || str[i] == 'Z')
+            str[i] -= 31;
+        else if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
+            str[i]++;
+    }
+}
+
+int main(int argc, char **argv)
+{
+    int serv_sock, clnt_sock;
+    struct sockaddr_in serv_addr, clnt_addr;
+    socklen_t clnt_addr_size;
+    char buffer[BUF_SIZE];
+
+    if (argc != 2)
+    {
+        printf("Usage: %s <Port>\n", argv[0]);
+        return 1;
+    }
+
+    serv_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (serv_sock == -1)
+    {
+        perror("socket");
+        return 1;
+    }
+
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(atoi(argv[1]));
+
+    if (bind(serv_sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1)
+    {
+        perror("bind");
+        close(serv_sock);
+        return 1;
+    }
+
+    if (listen(serv_sock, 5) == -1)
+    {
+        perror("listen");
+        close(serv_sock);
+        return 1;
+    }
+
+    clnt_addr_size = sizeof(clnt_addr);
+    clnt_sock = accept(serv_sock, (struct sockaddr *)&clnt_addr, &clnt_addr_size);
+    if (clnt_sock == -1)
+    {
+        perror("accept");
+        close(serv_sock);
+        return 1;
+    }
+
+    memset(buffer, 0, BUF_SIZE);
+    if (recv(clnt_sock, buffer, BUF_SIZE - 1, 0) == -1)
+    {
+        perror("recv");
+        close(clnt_sock);
+        close(serv_sock);
+        return 1;
+    }
+
+    process_string(buffer);
+
+    if (send(clnt_sock, buffer, strlen(buffer), 0) == -1)
+    {
+        perror("send");
+        close(clnt_sock);
+        close(serv_sock);
+        return 1;
+    }
+
+    close(clnt_sock);
+    close(serv_sock);
+    return 0;
+}
